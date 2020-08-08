7f4778c033283e334a76398e62d9d4a8d39285a6
YavorskiE <YavorskiE@outlook.com>
2020-08-08 03:50:58 -0400

    6.0.2. Заполнение массива. [#326637]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -6,5 +6,15 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100501];
         float[] prices = new float[40];
+        String[] names = new String[4];
+
+        names[0] = "Petr Arsentev";
+        names[1] = "Yevgeniy";
+        names[2] = "Yavorsky";
+        names[3] = "Яворский";
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
