be6f334f9609e20041bcc66738b2aebad4f1502d
YavorskiE <YavorskiE@outlook.com>
2020-08-08 04:47:41 -0400

    6.0.5. Массивы в тестах. [#326639]

--- test/ru/job4j/array/SquareTest.java
+++ test/ru/job4j/array/SquareTest.java
@@ -15,7 +15,7 @@ public class SquareTest {
     }
 
     @Test
-    public void whenBound5Then014916() {
+    public void whenBound5Then01496() {
         int bound = 5;
         int[] rst = Square.calculate(bound);
         int[] expect = new int[] {0, 1, 4, 9, 16};
