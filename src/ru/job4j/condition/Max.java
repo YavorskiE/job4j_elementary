eb04048be9147f6f44c859ceae95df478d0782d8
YavorskiE <YavorskiE@outlook.com>
2020-10-07 01:04:21 -0400

    3. Перегрузить метод max для трех чисел [#326792]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,13 +1,21 @@
 package ru.job4j.condition;
 
-public class    Max {
+public class Max {
     public static int max(int left, int right) {
-        int result = left > right ?  left : right;
-        return result;
+        return left >= right ?  left : right;
+    }
+
+    public static int max(int first, int second, int third) {
+        return Max.max(Max.max(first, second), third);
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        return Max.max(Max.max(first, second, third), fourth);
     }
 
     public static void main(String[] args) {
         int rsl = max(10, 9);
         System.out.println(rsl);
+        System.out.println(max(9, 10, 11, 12));
     }
 }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -28,4 +28,44 @@ public class MaxTest {
         int out = Max.max(left, right);
         Assert.assertEquals(expected, out);
     }
+
+    @Test
+    public void when10And10And11() {
+        int  first = 10, second = 10, third = 11;
+        int expected = 11;
+        int out = Max.max(first, second, third);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void when10And12And10() {
+        int  first = 10, second = 12, third = 10;
+        int expected = 12;
+        int out = Max.max(first, second, third);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void when10And9And9() {
+        int  first = 10, second = 9, third = 9;
+        int expected = 10;
+        int out = Max.max(first, second, third);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void when10And9And9and9() {
+        int  first = 10, second = 9, third = 9, fourth = 9;
+        int expected = 10;
+        int out = Max.max(first, second, third, fourth);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void when9And10And11and12() {
+        int  first = 9, second = 10, third = 11, fourth = 12;
+        int expected = 12;
+        int out = Max.max(first, second, third, fourth);
+        Assert.assertEquals(expected, out);
+    }
 }
